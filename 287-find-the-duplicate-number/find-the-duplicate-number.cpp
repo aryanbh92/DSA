1b30a192fdc3fdd296811cfef71f8c0cf8dfb66e
aryanbh92 <aryanbhadani922@gmail.com>
2026-08-31 13:58:45 +0530

    Time: 73 ms (11.85%) | Memory: 87.8 MB (19.82%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-    unordered_set<int> s;
-    for(int num : nums) {
-        if(s.find(num) != s.end()) return num;
-        s.insert(num);
-    }
-    return -1; // should never happen
-}
+        // Create an unordered_set to store numbers we've already seen
+        unordered_set<int> s;
 
+        // Loop through each number in the input array
+        for(int val : nums) {
+            // Check if the current number is already in the set
+            if(s.find(val) != s.end()) 
+                return val;  // If yes, it's the duplicate → return it immediately
+
+            // Otherwise, insert the number into the set for future checks
+            s.insert(val);
+        }
+
+        // If no duplicate is found (though problem guarantees one exists), return -1
+        return -1; 
+    }
 };
